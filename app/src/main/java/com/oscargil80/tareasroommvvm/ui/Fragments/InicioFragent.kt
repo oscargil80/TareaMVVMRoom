a3e8629a8764d98f3e35cdc85340a7e2181dec2a
Oscar Medina <oscargil80@gmail.com>
2024-09-26 23:28:05 -0400

    Segundo proyecto

--- app/src/main/java/com/oscargil80/tareasroommvvm/ui/Fragments/InicioFragent.kt
+++ app/src/main/java/com/oscargil80/tareasroommvvm/ui/Fragments/InicioFragent.kt
@@ -1,6 +1,7 @@
 package com.oscargil80.tareasroommvvm.ui.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,12 +35,21 @@ class InicioFragent : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.rvPaises.layoutManager = LinearLayoutManager(requireContext())
+/*        viewModel.getAllPersonal().observe(viewLifecycleOwner) { personalList ->
+            binding.rvPersonal.layoutManager = LinearLayoutManager(requireContext())
+            adapter = PersonalAdapter(personalList, this)
+            binding.rvPersonal.adapter = adapter
+        }*/
+
+
+
 
         viewModel.getPaises().observe(viewLifecycleOwner){   paisesList->
+            binding.rvPaises.layoutManager = LinearLayoutManager(requireContext())
             val adapter = PaisesAdapter(paisesList)
             binding.rvPaises.adapter = adapter
         }
+
 
 
         binding.addPaises.setOnClickListener {
